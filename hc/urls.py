c9e0cb8e9d1022f5b9280eb91c7bbd65f6c533b1
MH-sabu <sabu.prof@gmail.com>
2019-05-16 20:13:52 +0600

    pupet added23445

--- hc/urls.py
+++ hc/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path("", include("hc.api.urls")),
     path("", include("hc.front.urls")),
     path("", include("hc.payments.urls")),
-    path(r'',include(puput_urls)),
-]
+    path('', include('puput.urls')),
+    ]
 
 
 
