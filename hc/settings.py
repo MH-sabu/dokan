81e009262fda947eb4f9e7182546e0be286de1c7
MH-sabu <sabu.prof@gmail.com>
2019-05-17 04:04:16 +0600

    acbddd

--- hc/settings.py
+++ hc/settings.py
@@ -30,7 +30,7 @@ def envint(s, default):
 ALLOWED_HOSTS = ['178.128.56.243']
 
 SECRET_KEY = '&kb&!=@kg$ig#ex#tyw7*ow%c0=crwnarv1j31ebc678*tj4uc'
-DEBUG = True
+DEBUG = False
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "healthchecks@example.org")
 SUPPORT_EMAIL = os.getenv("SUPPORT_EMAIL")
 USE_PAYMENTS = envbool("USE_PAYMENTS", "False")
@@ -151,6 +151,10 @@ PING_ENDPOINT = os.getenv("PING_ENDPOINT", SITE_ROOT + "/ping/")
 PING_EMAIL_DOMAIN = os.getenv("PING_EMAIL_DOMAIN", "localhost")
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 
 COMPRESS_OFFLINE = True
 COMPRESS_CSS_HASHING_METHOD = "content"
