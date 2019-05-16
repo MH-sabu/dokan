53f606c5fa457600bf024d0bb5740eb77ddeb51b
MH-sabu <sabu.prof@gmail.com>
2019-05-17 00:33:41 +0600

    abcdsddlssdlddkd

--- hc/settings.py
+++ hc/settings.py
@@ -151,7 +151,6 @@ PING_ENDPOINT = os.getenv("PING_ENDPOINT", SITE_ROOT + "/ping/")
 PING_EMAIL_DOMAIN = os.getenv("PING_EMAIL_DOMAIN", "localhost")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static-collected")
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
