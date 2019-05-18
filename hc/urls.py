cc9936e6c9732a8cbde35bf224a20b08cbbd135e
MH-sabu <sabu.prof@gmail.com>
2019-05-18 22:30:24 +0600

    sabudds

--- hc/urls.py
+++ hc/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
+
 
 from hc.accounts import views as accounts_views
 
